6f1114b4b7ab8cb55c7b36f65be70db747e9e97a
Justin Davis <amoamare@msn.com>
2023-12-19 09:47:16 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleServiceTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1048, 723);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Console Service Tool (C.S.T)";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
